12315ff95221103f68787b6fb7a1ffe77fbd6d8f
Ygor Guilherme <100237670+ygorfsguilherme@users.noreply.github.com>
2023-12-30 12:21:13 -0300

    Create install-config.sh

---
+++ install-config.sh
@@ -0,0 +1,13 @@
+#!bin/bash
+
+apt install nginx certbot python3-certbot-nginx firewalld
+
+sudo ufw disable
+sudo apt remove --purge ufw
+
+sudo systemctl start firewalld
+sudo systemctl enable firewalld
+sudo firewall-cmd --add-service=ssh --permanent
+sudo firewall-cmd --add-service=http --permanent
+sudo firewall-cmd --add-service=https --permanent
+sudo firewall-cmd --reload
