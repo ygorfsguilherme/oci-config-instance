52b39f0ac9a02d3fd62af17d2edc67e8d335b092
Ygor Guilherme <100237670+ygorfsguilherme@users.noreply.github.com>
2025-02-04 13:42:04 -0300

    Fix erro install web

--- install-config.sh
+++ install-config.sh
@@ -5,14 +5,6 @@
 # Disponível para Ubuntu 22.04, preferencialmente a versão minimal
 #######################################################################################
 
-if [[ "$PWD" == */ocpi ]]; then
-    source ./menu_main.sh
-    echo "Está no diretório ocpi."
-else
-    source ./ocpi/menu_main.sh
-    echo "Não está no diretório ocpi."
-fi
-
 # Função para checar e instalar pacotes
 check_and_install() {
     if ! dpkg -l | grep -q $1; then
@@ -24,7 +16,6 @@ check_and_install() {
 
 first_exec() {
     if [ ! -f exec ]; then
-        source ./menu_main.sh
         sudo apt update -y
         check_and_install unzip
         check_and_install curl
@@ -33,8 +24,8 @@ first_exec() {
         echo "Instalação de dependências concluída."
 
         echo "Download do script de configuração..."
-        curl -s -O https://github.com/ygorfsguilherme/oci-config-instance/archive/refs/tags/0.0.1.zip
-        unzip 0.0.1.zip -d opci
+        curl -s -O https://github.com/ygorfsguilherme/oci-config-instance/archive/refs/tags/release.zip
+        unzip release.zip -d opci
         
         touch exec
         touch ./opci/exec
@@ -45,6 +36,15 @@ first_exec() {
 }
 
 first_exec
+
+
+if [[ "$PWD" == */ocpi ]]; then
+    source ./ocpi/menu_main.sh
+    echo "Está no diretório ocpi."
+else
+    source ./menu_main.sh
+    echo "Não está no diretório ocpi."
+fi
 
 # Início do loop
 while true; do
